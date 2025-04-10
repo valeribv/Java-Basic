fb5f8adfa6aa0ec6f0d4616f8fe2c83651ed559c
Admin <valeri_bv@mail.bg>
2025-04-10 19:35:02 +0300

    Add if case in Cinema task

--- Java/src/Cinema.java
+++ Java/src/Cinema.java
@@ -10,20 +10,33 @@ public class Cinema {
         double rows = Double.parseDouble(scanner.nextLine());
         double price = 0;
 
-        switch (cinemaType){
-            case "Premiere":
-                price = colum * rows;
-                System.out.printf("%.2f lv", price * 12);
-                break;
-            case "Normal":
-                price = colum * rows;
-                System.out.printf("%.2f lv", price * 7.5);
-                break;
-            case "Discount":
-                price = colum * rows;
-                System.out.printf("%.2f lv", price * 5);
-                break;
-            default: System.out.println("error");
+//        switch (cinemaType){
+//            case "Premiere":
+//                price = colum * rows;
+//                System.out.printf("%.2f lv", price * 12);
+//                break;
+//            case "Normal":
+//                price = colum * rows;
+//                System.out.printf("%.2f lv", price * 7.5);
+//                break;
+//            case "Discount":
+//                price = colum * rows;
+//                System.out.printf("%.2f lv", price * 5);
+//                break;
+//            default: System.out.println("error");
+
+        if (cinemaType.equals("Premiere")) {
+            price = (colum * rows) * 12;
+            System.out.printf("%.2f" , price);
+            System.out.println(" leva");
+        } else if (cinemaType.equals("Normal")) {
+            price = (colum * rows) * 7.5;
+            System.out.printf("%.2f", price);
+            System.out.println(" leva");
+        } else if (cinemaType.equals("Discount")) {
+            price = (colum * rows) * 5;
+            System.out.printf("%.2f", price);
+            System.out.println(" leva");
         }
     }
 }
--- Java/src/SummerOutfit.java
+++ Java/src/SummerOutfit.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class SummerOutfit {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+
+
+
+    }
 }
