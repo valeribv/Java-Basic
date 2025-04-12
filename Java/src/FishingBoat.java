9e2b7d2c589079a9cb99cb82564c4a0d7fbd8001
Admin <valeri_bv@mail.bg>
2025-04-12 11:16:51 +0300

    FishBoat fix all code.

--- Java/src/FishingBoat.java
+++ Java/src/FishingBoat.java
@@ -14,60 +14,36 @@ public class FishingBoat {
         switch (season) {
             case "Spring":
                 priceShip = 3000;
-                if (peopleCount <= 6 && peopleCount % 2 == 0) {
-                    priceShip = (priceShip * 0.9) * 0.05;
-                } else if (peopleCount >= 7 && peopleCount <= 11 && peopleCount % 2 == 0) {
-                    priceShip = (priceShip * 0.85) * 0.05;
-                } else if (peopleCount > 12 && peopleCount % 2 == 0) {
-                    priceShip = (priceShip * 0.85) * 0.05;
-                } else if (peopleCount <= 6) {
-                    priceShip = (priceShip * 0.9);
-                } else if (peopleCount >= 7 && peopleCount <= 11) {
-                    priceShip = (priceShip * 0.85);
-                } else if (peopleCount > 12) {
-                    priceShip = (priceShip * 0.85);
-                    break;
-                }
+                break;
             case "Summer":
+                priceShip = 4200;
+                break;
             case "Autumn":
                 priceShip = 4200;
-                if (peopleCount <= 6 && peopleCount % 2 == 0 && !season.equals("Autumn")) {
-                    priceShip = (priceShip * 0.9) * 0.05;
-                } else if (peopleCount >= 7 && peopleCount <= 11 && peopleCount % 2 == 0 && !season.equals("Autumn")) {
-                    priceShip = (priceShip * 0.85) * 0.05;
-                } else if (peopleCount > 12 && peopleCount % 2 == 0 && !season.equals("Autumn")) {
-                    priceShip = (priceShip * 0.85) * 0.05;
-                } else if (peopleCount <= 6) {
-                    priceShip = (priceShip * 0.9);
-                } else if (peopleCount >= 7 && peopleCount <= 11) {
-                    priceShip = (priceShip * 0.85);
-                } else if (peopleCount > 12) {
-                    priceShip = (priceShip * 0.85);
-                    break;
-                }
+
+                break;
             case "Winter":
                 priceShip = 2600;
-                if (peopleCount <= 6 && peopleCount % 2 == 0) {
-                    priceShip = (priceShip * 0.9) * 0.05;
-                } else if (peopleCount >= 7 && peopleCount <= 11 && peopleCount % 2 == 0) {
-                    priceShip = (priceShip * 0.85) * 0.05;
-                } else if (peopleCount > 12 && peopleCount % 2 == 0) {
-                    priceShip = (priceShip * 0.85) * 0.05;
-                } else if (peopleCount <= 6) {
-                    priceShip = (priceShip * 0.9);
-                } else if (peopleCount >= 7 && peopleCount <= 11) {
-                    priceShip = (priceShip * 0.85);
-                } else if (peopleCount > 12) {
-                    priceShip = (priceShip * 0.85);
-                    break;
-                }
+                break;
         }
-        if (budgedPeople < priceShip) {
-            double neededMoney = Math.abs(priceShip - budgedPeople);
-            System.out.printf("Not enough money! You need %.2f leva.", neededMoney);
+        if (peopleCount <= 6) {
+            priceShip *= 0.9;
+        } else if (peopleCount >= 7 && peopleCount <= 11) {
+            priceShip *= 0.85;
+
+        } else if (peopleCount > 12) {
+            priceShip *= 0.75;
+        }
+        if (peopleCount % 2 == 0 && !season.equals("Autumn")) {
+            priceShip *= 0.95;
+        }
+
+        double leftMoney = Math.abs(budgedPeople - priceShip);
+
+        if (priceShip > budgedPeople) {
+            System.out.printf("Not enough money! You need %.2f leva.", leftMoney);
         } else {
-            double neededMoney = Math.abs(priceShip - budgedPeople);
-            System.out.printf("Yes! You have %.2f leva left.", neededMoney);
+            System.out.printf("Yes! You have %.2f leva left.", leftMoney);
         }
     }
 }
