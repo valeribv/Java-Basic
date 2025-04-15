984276c26eaa55a0886adbc86dba33cbe55b6439
Admin <valeri_bv@mail.bg>
2025-04-15 23:45:37 +0300

    FIx program OddOrEvenSum. Removed unused variables + print fix.

--- Java/src/OddEvenSum.java
+++ Java/src/OddEvenSum.java
@@ -9,6 +9,7 @@ public class OddEvenSum {
 
         int quantityOfNumbers = Integer.parseInt(scanner.nextLine());
 
+        //Variables for construction
         int sumEvenLeft = 0;
         int sumEvenRight = 0;
         int sumOddLeft = 0;
@@ -17,43 +18,31 @@ public class OddEvenSum {
         int leftSide = 0;
         int rightSide = 0;
 
-        int evenLeft = 0;
-        int oddLeft = 0;
-
-        int evenRight = 0;
-        int oddRight = 0;
-
+        //input for quantity of numbers
         for (int index = 1; index <= quantityOfNumbers; index++) {
 
             int inputNumber = Integer.parseInt(scanner.nextLine());
 
             if (index % 2 == 0) {
-
                 leftSide = inputNumber;
-                //     четно / нечетно
                 // count odd or even for LEFT side
                 if (leftSide % 2 != 0) {
-                    evenLeft = leftSide;
-                    sumEvenLeft += evenLeft;
+                    sumEvenLeft += leftSide;
                 } else {
-                    oddLeft = leftSide;
-                    sumOddLeft += oddLeft;
+                    sumOddLeft += leftSide;
                 }
 
             } else {
-
                 rightSide = inputNumber;
-                //     четно / нечетно
                 // count odd or even for RIGHT side
                 if (rightSide % 2 != 0) {
-                    evenRight = rightSide;
-                    sumEvenRight += evenRight;
+                    sumEvenRight += rightSide;
                 } else {
-                    oddRight = rightSide;
-                    sumOddRight += oddRight;
+                    sumOddRight += rightSide;
                 }
 
             }
+            //Print
             if (index == quantityOfNumbers) {
 
                 if (sumOddRight == sumOddLeft) {
