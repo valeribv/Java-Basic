a0bf23762bbe8e9e7bbf5857afdb1778953a25ec
Admin <valeri_bv@mail.bg>
2025-04-23 19:45:15 +0300

    Fix Oskars

--- Java/src/Oscars.java
+++ Java/src/Oscars.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Oscars {
 
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
 
         String actorName = scanner.nextLine();
         double pointsFromAcademy = Double.parseDouble(scanner.nextLine());
@@ -25,15 +23,20 @@ public class Oscars {
             for (int j = 0; j <= JuryName.length() - 1; j++) {
                 pointFromJuryName++;
             }
-            countPoints = ((pointFromJuryName * pointsFromJury) / 2);
-            pointsFromAcademy += countPoints;
+            countPoints =pointsFromAcademy + ((pointFromJuryName * pointsFromJury) / 2);
+            pointsFromAcademy =+ countPoints;
             sumPoints = countPoints;
             pointFromJuryName = 0;
+
+            if ( sumPoints >= finalPoints){
+                break;
+            }
         }
-        if (sumPoints >= 0 || sumPoints <= finalPoints) {
-            System.out.printf("Sorry, %s you need %.2f more!", actorName, (finalPoints - sumPoints));
+        if (sumPoints < finalPoints) {
+            System.out.printf("Sorry, %s you need %.1f more!", actorName, (finalPoints - sumPoints));
         } else {
-            System.out.printf("Congratulations, %s got a nominee for leading role with %.2f!", actorName, (finalPoints - sumPoints));
+            System.out.printf("Congratulations, %s got a nominee for leading role with %.1f!", actorName,
+                    sumPoints);
         }
     }
 }
